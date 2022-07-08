f90e65ebb6e3fbe681de4b1f085faaa69e612c92
Simeon Atanasov <atanasov.simeon3@gmail.com>
2022-07-08 13:49:45 +0300

    Revert "commitvam changes"
    This reverts commit 556d9a08e3227721855727ad65c5b771cab95e87.

--- camtastic-application/DatabaseHandler.cs
+++ camtastic-application/DatabaseHandler.cs
@@ -13,9 +13,6 @@ namespace camtastic_application
     class DatabaseHandler
     {
         MySqlConnection conn;
-        /// <summary>
-        /// establishes a connection to the database
-        /// </summary>
         public void Connect() //method to connect to SQL database
         {
 
@@ -30,33 +27,11 @@ namespace camtastic_application
                 Console.WriteLine("There is an Error.");
             }
         }
-        /// <summary>
-        /// used to add a new row of data
-        /// </summary>
         public void AddData(int rating, string cameraBrand, string cameraModel, string url) //method to add a new photo to the database
         {
             string sqlQuery = $"INSERT INTO `photo` VALUES ('{url}', '{cameraBrand}', '{cameraModel}', '{rating}')"; //ive adjusted the table slightly, ive sent you the code to create one.
             MySqlCommand cmd = new MySqlCommand(sqlQuery, conn);
             cmd.ExecuteNonQuery(); //executes command
-        }
-        /// <summary>
-        /// used to grab every element from a row
-        /// </summary>
-        public (string, string, string, int) GrabData(int rowNum)
-        {
-            string sqlQuery = $"SELECT * FROM `photo` WHERE URL='https://photo-forum.net/i/'" + rowNum;
-            MySqlCommand cmd = new MySqlCommand(sqlQuery, conn);
-            MySqlDataReader reader = cmd.ExecuteReader();
-            return (reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetInt32(3)); //returns url, camerbrand, cameramodel and rating after executing MySqlDataReader
-        }
-        /// <summary>
-        /// used to get amount of data currently present in database
-        /// </summary>
-        public int FindDataAmount()
-        {
-            string sqlQuery2 = $"SELECT COUNT(*) FROM `photo`"; 
-            MySqlCommand cmd2 = new MySqlCommand(sqlQuery2, conn);
-            return (int)cmd2.ExecuteScalar(); //returns the amount of rows
-        }
+        }    
     }
 }
