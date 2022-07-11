88529a59077e0f51fc27bfccba965c03fae2b774
Simeon Atanasov <atanasov.simeon3@gmail.com>
2022-07-11 10:08:37 +0300

    added pieChart

--- camtastic-application/MainWindow.xaml.cs
+++ camtastic-application/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ using System.Text.RegularExpressions;
 using System.Diagnostics;
 using LiveCharts;
 using LiveCharts.Wpf;
+using LiveCharts.Defaults;
+
 
 namespace camtastic_application
 {
@@ -31,44 +33,56 @@ namespace camtastic_application
     {
         readonly MainWindowViewModel methodExtender = new MainWindowViewModel();
 
+        static Random random;
         public MainWindow()
         {
+
             InitializeComponent();
+            random = new Random();
 
-            SeriesCollection = new SeriesCollection
-            {
-                new ColumnSeries
-                {
-                    Title = "2015",
-                    Values = new ChartValues<double> { 10, 50, 39, 50 }
-                }
-            };
-
-            //adding series will update and animate the chart automatically
-            SeriesCollection.Add(new ColumnSeries
-            {
-                Title = "2016",
-                Values = new ChartValues<double> { 11, 56, 42 }
-            });
-
-            //also adding values updates and animates the chart automatically
-            SeriesCollection[1].Values.Add(48d);
-
-            Labels = new[] { "Maria", "Susan", "Charles", "Frida" };
-            Formatter = value => value.ToString("N");
-
-            DataContext = this;
+            Brand.Text = "Sony";
+            Model.Text = "Sony";
         }
 
         public SeriesCollection SeriesCollection { get; set; }
-        public string[] Labels { get; set; }
-        public Func<double, string> Formatter { get; set; }
 
+        private void ButtonChangePieChart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataContext = new object();
 
+                SeriesCollection = new SeriesCollection
+                {
+                    new PieSeries
+                    {
+                        Title = "1. Instagram",
+                        Values = new ChartValues<ObservableValue> { new ObservableValue(random.Next(50000, 650000))},
+                        DataLabels = true
+                    },new PieSeries
+                    {
+                        Title = "2. Twitter",
+                        Values = new ChartValues<ObservableValue> { new ObservableValue(random.Next(45000, 400000))},
+                        DataLabels = true
+                    },new PieSeries
+                    {
+                        Title = "3. YouTube",
+                        Values = new ChartValues<ObservableValue> { new ObservableValue(random.Next(30000, 800000))},
+                        DataLabels = true
+                    }
+                };
+                DataContext = this;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Pasta Grafiği Oluşturulamadı \n" + exp.Message);
+            }
 
-        /// <summary>
-        /// event handler for getInfo button click
-        /// </summary>
+            /// <summary>
+            /// event handler for getInfo button click
+            /// </summary>
+
+        }
         private void GetInfoButton_Click(object sender, RoutedEventArgs e)
         {
             if (ThreadPool.PendingWorkItemCount == 0)
@@ -76,10 +90,19 @@ namespace camtastic_application
                 getInfoButton.Content = "Getting info, please wait."; //if no process is being worked on, we change the text
                 methodExtender.GetInfo();
 
+
+
             }
+        }
+
+        private void DefaultLegend_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
+    
+
 
         
         
